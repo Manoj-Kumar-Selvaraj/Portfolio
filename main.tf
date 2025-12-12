826695228364b39c978a910ce3701cc126883686
Manoj Kumar <ss.mano1998@gmail.com>
2025-12-13 01:16:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -208,15 +208,3 @@ resource "azurerm_key_vault_access_policy" "vm_kv_policy" {
 
   secret_permissions = ["Get", "List"]
 }
-
-############################################
-# Admin user KV access policy
-############################################
-
-resource "azurerm_key_vault_access_policy" "admin_kv_policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = ["Get", "List", "Set", "Delete"]
-}
