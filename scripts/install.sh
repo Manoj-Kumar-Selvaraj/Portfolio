cca85c23fbee7f525eebf1607998dc70cfdc4fd1
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-12-21 22:24:41 +0530

    Added az installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,11 @@ chmod +x ./scripts/*.sh
 ./scripts/install-idle-shutdown-service.sh
 ./scripts/setup-nginx-jenkins.sh
 ./scripts/az.sh
-./scripts/trigger_job_local.sh "initial-setup"
+if ./scripts/fetch_jenkins_token.sh | grep -q ':'; then
+  ./scripts/trigger_job_local.sh initial-setup
+else
+  echo "Jenkins credentials not ready. Skipping trigger."
+fi
 ./scripts/install-idle-shutdown-service.sh
 
 echo "Installation completed successfully"
