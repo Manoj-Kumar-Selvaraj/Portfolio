21e9f19e00419fee79de1fa0d99518c6e1bf9e53
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-12-21 16:55:30 +0530

    Cloud-init

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,8 @@ set -euo pipefail
 
 echo "Starting Jenkins on-demand installation"
 
-
-./scripts/setup-docker-compose-harbour.sh
-./scripts/jenkins.sh
+./scripts/setup-jenkins-agent.sh
+./scripts/jenkins-controller.sh
 ./scripts/install-idle-shutdown-service.sh
 ./scripts/setup-nginx-jenkins.sh
 
