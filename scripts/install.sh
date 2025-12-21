5ae5e89cf484d84d7c69f158715b23b03a1f05ae
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-12-21 18:01:09 +0530

    Changed to ubuntu 24

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ set -x
 echo "Starting Jenkins on-demand installation"
 chmod +x ./scripts/*.sh
 ./scripts/setup-jenkins-agent.sh
-./scripts/jenkins-controller.sh
+./scripts/setup-jenkins-controller.sh
 ./scripts/install-idle-shutdown-service.sh
 ./scripts/setup-nginx-jenkins.sh
 
